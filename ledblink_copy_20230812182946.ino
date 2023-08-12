d4f656bc5720fdc96a6125bf911ba24606ec9f4c
Azraritgithub <133782181+Azraritgithub@users.noreply.github.com>
2023-08-12 19:12:41 +0530

    Add files via upload

---
+++ ledblink_copy_20230812182946.ino
@@ -0,0 +1,22 @@
+
+int LED_1 = 5;
+
+void setup ()
+ {
+   // put your setup code here, to run
+   pinMode (LED_1,  OUTPUT);
+
+
+
+
+ }
+
+
+ void loop()
+ {
+   digitalWrite(LED_1, HIGH);
+   // put your main code here, to run
+   
+ } 
+
+
